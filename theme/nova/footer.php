41c4245c40ea6642f297ac8a1d6a3a1ba03719af
Geek377 <geek377@gmail.com>
2012-01-23 18:30:42 -0600

    Adding footer
    Added the site generator for now (the copyright line)
    Will add fat footer menus next

---
+++ theme/nova/footer.php
@@ -0,0 +1,17 @@
+<?php
+?>
+
+	</div><!-- #main -->
+
+	<footer id="colophon" role="contentinfo">
+			<div id="site-generator">
+				<?php do_action( 'twentyeleven_credits' ); ?>
+				<a href="<?php echo esc_url( __( 'http://wildfiregames.com', 'twentyeleven' ) ); ?>" rel="generator"><?php printf( __( '&copy; 2012 Wildfire Games | CC-BY-SA ', 'twentyeleven' ) ); ?></a>
+			</div>
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
