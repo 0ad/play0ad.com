0946e86b8ecc3059c9c8a7f2d796ce52e203cdc5
Geek377 <geek377@gmail.com>
2012-01-26 16:11:57 -0600

    Footer & others
    Mainly footer.php [completed except last content column]
    And others as well

--- theme/nova/footer.php
+++ theme/nova/footer.php
@@ -2,10 +2,31 @@
 ?>
 	</div><!-- #main -->
 	<footer id="colophon" role="contentinfo">
-			<div id="site-generator">
-				<?php do_action( 'twentyeleven_credits' ); ?>
-				<a href="<?php echo esc_url( __( 'http://wildfiregames.com', 'twentyeleven' ) ); ?>" rel="generator"><?php printf( __( '&copy; 2012 Wildfire Games | CC-BY-SA ', 'twentyeleven' ) ); ?></a>
+		<div id="supplementary" class="five">
+			<div id="first" class="widget-area" role="complementary">
+				<h2>The Game</h2>
+				<?php wp_nav_menu( array('menu' => 'Game', 'container' => '' , 'menu_class' => 'Game', 'menu_id' =>'' )); ?>
 			</div>
+			<div id="second" class="widget-area" role="complementary">
+				<h2>News</h2>
+				<?php wp_nav_menu( array('menu' => 'News', 'container' => '' , 'menu_class' => 'News', 'menu_id' =>'' )); ?>
+			</div>
+			<div id="third" class="widget-area" role="complementary">
+				<h2>Media</h2>
+				<?php wp_nav_menu( array('menu' => 'Media', 'container' => '' , 'menu_class' => 'Media', 'menu_id' =>'' )); ?>
+			</div>
+			<div id="fourth" class="widget-area" role="complementary">
+				<h2>Community</h2>
+				<?php wp_nav_menu( array('menu' => 'Community', 'container' => '' , 'menu_class' => 'Community', 'menu_id' =>'' )); ?>
+			</div>
+			<div id="fifth" class="widget-area" role="complementary">
+				<p>Content coming soon...</p>
+			</div>
+		</div>
+		<div id="site-generator">
+			<?php do_action( 'twentyeleven_credits' ); ?>
+			<a href="<?php echo esc_url( __( 'http://wildfiregames.com', 'twentyeleven' ) ); ?>" rel="generator"><?php printf( __( '&copy; 2012 Wildfire Games | CC-BY-SA ', 'twentyeleven' ) ); ?></a>
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 <?php wp_footer(); ?>
