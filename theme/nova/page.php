4cea4e2deaacb7930f71a1c51943efce88f53b94
Geek377 <geek377@gmail.com>
2012-01-28 19:22:08 -0600

    More work
    Did some more customizing on files

--- theme/nova/page.php
+++ theme/nova/page.php
@@ -6,26 +6,16 @@
  * Please note that this is the WordPress construct of pages
  * and that other 'pages' on your WordPress site will use a
  * different template.
- *
- * @package WordPress
- * @subpackage Twenty_Eleven
- * @since Twenty Eleven 1.0
  */
 
 get_header(); ?>
 
 		<div id="primary">
 			<div id="content" role="main">
-
-				<?php while ( have_posts() ) : the_post(); ?>
-
-					<?php get_template_part( 'content', 'page' ); ?>
-
-					<?php comments_template( '', true ); ?>
-
-				<?php endwhile; // end of the loop. ?>
-
 			</div><!-- #content -->
 		</div><!-- #primary -->
-
+		<div id="secondary" class="widget-area" role="complementary">
+			<?php get_sidebar; ?> 
+		</div>
+		
 <?php get_footer(); ?>
---
+++ theme/nova/sidebar.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The Sidebar containing the main widget area.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+
+$options = twentyeleven_get_theme_options();
+$current_layout = $options['theme_layout'];
+
+if ( 'content' != $current_layout ) :
+?>
+		<div id="secondary" class="widget-area" role="complementary">
+			<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
+
+				<aside id="archives" class="widget">
+					<h3 class="widget-title"><?php _e( 'Archives', 'twentyeleven' ); ?></h3>
+					<ul>
+						<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
+					</ul>
+				</aside>
+
+				<aside class="widget widget_rss" id="rss-2">
+					<h3 class="widget-title">
+						<a title="Syndicate this content" href="http://svn.wildfiregames.com/log.atom" class="rsswidget">
+							<img width="14" height="14" alt="RSS" src="http://www.wildfiregames.com/0adwebdev/theme-dev/wp-includes/images/rss.png" style="border:0">
+						</a>
+						<a title="" href="http://www.wildfiregames.com/0ad/" class="rsswidget">Revision Log</a>
+					</h3>
+				</aside>
+
+			<?php endif; // end sidebar widget area ?>
+		</div><!-- #secondary .widget-area -->
+<?php endif; ?>
