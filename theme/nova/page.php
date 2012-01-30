c8920ad85dfef88578120fd9b496c8aa19e23348
Brian Stempin <brian.stempin@gmail.com>
2012-01-30 18:23:36 -0500

    Restored page.php to it's default self.

--- theme/nova/page.php
+++ theme/nova/page.php
@@ -16,6 +16,12 @@ get_header(); ?>
 
 		<div id="primary">
 			<div id="content" role="main">
+			
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'content', 'page' ); ?>
+
+				<?php endwhile; // end of the loop. ?>
 			</div><!-- #content -->
 		</div><!-- #primary -->
 		<div id="secondary" class="widget-area" role="complementary">
