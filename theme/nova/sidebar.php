22c8b086fb20ef77a667a133408572689a918e30
Geek377 <geek377@gmail.com>
2012-01-30 18:59:37 -0600

    Menu width
    Also testing sidebar

--- theme/nova/sidebar.php
+++ theme/nova/sidebar.php
@@ -16,19 +16,10 @@ if ( 'content' != $current_layout ) :
 			<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
 
 				<aside id="archives" class="widget">
-					<h3 class="widget-title"><?php _e( 'Archives', 'twentyeleven' ); ?></h3>
+					<h3 class="widget-title"><?php _e( 'Archives', 'nova' ); ?></h3>
 					<ul>
 						<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
 					</ul>
-				</aside>
-
-				<aside class="widget widget_rss" id="rss-2">
-					<h3 class="widget-title">
-						<a title="Syndicate this content" href="http://svn.wildfiregames.com/log.atom" class="rsswidget">
-							<img width="14" height="14" alt="RSS" src="http://www.wildfiregames.com/0adwebdev/theme-dev/wp-includes/images/rss.png" style="border:0">
-						</a>
-						<a title="" href="http://www.wildfiregames.com/0ad/" class="rsswidget">Revision Log</a>
-					</h3>
 				</aside>
 
 			<?php endif; // end sidebar widget area ?>
