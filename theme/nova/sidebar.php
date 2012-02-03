5861f20688a9bc9afaa8f135619417cf585d5225
Geek377 <geek377@gmail.com>
2012-02-03 17:36:53 -0600

    Sidebar progress
    Added revision log and some code
    Added dev blog feed
    Styling these next

--- theme/nova/sidebar.php
+++ theme/nova/sidebar.php
@@ -15,13 +15,44 @@ if ( 'content' != $current_layout ) :
 		<div id="secondary" class="widget-area" role="complementary">
 			<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
 
-				<aside id="download" class="widget">
+				<aside id="download">
 					<p>Download Button</p>
 				</aside>
 				
-				<aside id="share" class="widget">
+				<aside id="share">
 					<p>Share Button</p>
 				</aside>
+				
+				<aside id="revlog" class="widget">
+					<h2><?php _e('Revision Log'); ?></h2>
+					<?php // Get RSS Feed(s)
+					include_once(ABSPATH . WPINC . '/feed.php');
+
+					$rss = fetch_feed('http://svn.wildfiregames.com/log.atom');
+					if (!is_wp_error( $rss ) ) : // Checks that the object is created correctly 
+   					$maxitems = $rss->get_item_quantity(3); 
+   					
+    				$rss_items = $rss->get_items(0, $maxitems); 
+					endif;
+					?>
+
+					<ul>
+    					<?php if ($maxitems == 0) echo '<li>No items.</li>';
+    					else
+    					// Loop through each feed item and display each item as a hyperlink.
+    					foreach ( $rss_items as $item ) : ?>
+    					<li>
+        					<a href='<?php echo esc_url( $item->get_permalink() ); ?>'
+        					title='<?php echo 'Updated '.$item->get_date('j F Y | g:i a'); ?>'>
+        					<?php echo esc_html( $item->get_title() ); ?></a>
+    					</li>
+    					<?php endforeach; ?>
+					</ul>
+				</aside>
+				
+				<aside id="devblog" class="widget">
+					<h2>Dev Blog</h2>
+					</aside>
 				
 				<aside id="donate" class="widget">
 					<p>Donate Section</p>
