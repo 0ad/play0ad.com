544ea85acd2ea728b20c32eb898962cf57b195ec
Brian Stempin <brian.stempin@gmail.com>
2012-02-25 19:33:23 -0500

    Revert back to 9fc6b6a3473b0e81a746eb225995a0c80d413415

--- theme/nova/front-page.php
+++ theme/nova/front-page.php
@@ -7,36 +7,9 @@ get_header(); ?>
 		<div id="primary">
 			<div id="content" role="main">
 
-			<?php if ( have_posts() ) : ?>
-
-				<?php twentyeleven_content_nav( 'nav-above' ); ?>
-
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
-
-					<?php get_template_part( 'content', get_post_format() ); ?>
-
-				<?php endwhile; ?>
-				<?php twentyeleven_content_nav( 'nav-below' ); ?>
-
-			<?php else : ?>
-
-				<article id="post-0" class="post no-results not-found">
-					<header class="entry-header">
-						<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentyeleven' ); ?></h1>
-					</header><!-- .entry-header -->
-
-					<div class="entry-content">
-						<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyeleven' ); ?></p>
-						<?php get_search_form(); ?>
-					</div><!-- .entry-content -->
-				</article><!-- #post-0 -->
-
-			<?php endif; ?>
 
 			</div><!-- #content -->
 		</div><!-- #primary -->
 
 <?php get_sidebar(); ?>
-<?php the_widget('Video'); ?>
 <?php get_footer(); ?>
