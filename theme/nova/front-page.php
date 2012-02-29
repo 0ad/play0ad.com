bc5c39e0e03e615d000991e97b5d0d9aab43d0b5
Geek377 <Mishra@iMac.local>
2012-02-29 17:38:25 -0600

    Front page
    Now in sync with server

--- theme/nova/front-page.php
+++ theme/nova/front-page.php
@@ -6,8 +6,11 @@ Front page template file
 get_header(); ?>
 		<div id="primary">
 			<div id="content" role="main">
+				<?php while ( have_posts() ) : the_post(); ?>
 
+				<?php get_template_part( 'content', 'page' ); ?>
 
+				<?php endwhile; // end of the loop. ?>
 			</div><!-- #content -->
 		</div><!-- #primary -->
 
