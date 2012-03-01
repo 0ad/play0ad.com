e56eb5d878cd353fd8d0d5f799b24e45e12b4af3
Geek377 <geek377@gmail.com>
2012-02-29 18:48:13 -0600

    Home Page
    A plethora of updates

--- theme/nova/front-page.php
+++ theme/nova/front-page.php
@@ -6,11 +6,25 @@ Front page template file
 get_header(); ?>
 		<div id="primary">
 			<div id="content" role="main">
-				<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'content', 'page' ); ?>
-
-				<?php endwhile; // end of the loop. ?>
+				<div id="carousel">
+				</div><!-- #carousel -->
+				<div id="front-tweet">
+				</div><!-- #front-tweet -->	
+				<div id="updates">
+				<?php
+					$args = array( 'numberposts' => 1);
+					$postslist = get_posts( $args );
+					foreach ($postslist as $post) :  setup_postdata($post); ?> 
+						<div>
+							<h1 class="front-blog-title"> 
+								<?php the_title(); ?>
+							</h1>
+							<div id="front-excerpt">
+								<?php the_excerpt(); ?>
+							</div><!-- #front-excerpt -->	
+						</div>
+				<?php endforeach; ?>
+				</div><!-- #updates -->				
 			</div><!-- #content -->
 		</div><!-- #primary -->
 
