284251397f64b3ba1a17ffbe0ed7584945598729
Geek377 <geek377@gmail.com>
2012-03-03 17:17:18 -0600

    Media Page
    Media Template, media template styling, new screenshot

---
+++ theme/nova/media-page.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Template Name: Media
+ * The template for displaying the media page in Nova
+ *
+ * @package WildfireGames
+ * @subpackage Nova
+ * @since Nova 0.3
+ */
+?>
+<?php get_header(); ?>
+		<div id="primary">
+			<div id="content" role="main">
+				<div id="media-slider">
+					<p>Slider goes here</p>
+				</div><!-- #media-slider -->
+				<div id="media-wall">
+					<a href="#" class="media-block">
+					</a>
+					<a href="#" class="media-block">
+					</a>
+					<a href="#" class="media-block">
+					</a>
+					<a href="#" class="media-block">
+					</a>
+					<a href="#" class="media-block">
+					</a>
+					<a href="#" class="media-block">
+					</a>
+				</div><!-- #media-wall -->
+			</div><!-- #content -->
+		</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
