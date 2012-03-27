33ec9a234997cb69ff9d2be9b10640604c5cf24c
Geek377 <geek377@gmail.com>
2012-03-27 12:35:54 -0500

    Menu Images
    The menu images are in place and almost ready to go, except for the
    child hover, still figuring that bit out. Also modified header.php to
    include search form and fix menu bug that caused the menu to collapse
    upon adding a new page. And the slider background is standard CSS now.

--- theme/nova/header.php
+++ theme/nova/header.php
@@ -77,14 +77,15 @@
 				else :
 			?>
 			<?php endif; ?>
-
+                         
 			<nav id="access" role="navigation">
 				<h3 class="assistive-text"><?php _e( 'Main menu', 'nova' ); ?></h3>
 				<?php /*  Allow screen readers / text browsers to skip the navigation menu and get right to the good stuff. */ ?>
 				<div class="skip-link"><a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to primary content', 'nova' ); ?>"><?php _e( 'Skip to primary content', 'nova' ); ?></a></div>
 				<div class="skip-link"><a class="assistive-text" href="#secondary" title="<?php esc_attr_e( 'Skip to secondary content', 'nova' ); ?>"><?php _e( 'Skip to secondary content', 'nova' ); ?></a></div>
 				<?php /* Our navigation menu.  If one isn't filled out, wp_nav_menu falls back to wp_page_menu. The menu assiged to the primary position is the one used. If none is assigned, the menu with the lowest ID is used. */ ?>
-				<?php wp_nav_menu( array( 'theme_location' => 'nova' ) ); ?>
+				<?php wp_nav_menu( array( 'menu' => 'MAIN' ) ); ?>
+				<?php get_search_form(); ?>
 			</nav><!-- #access -->
 	</header><!-- #branding -->
 
