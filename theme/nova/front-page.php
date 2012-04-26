401477028569009bb2b14a8461871bd4d6c50be4
Geek377 <geek377@gmail.com>
2012-04-25 21:21:02 -0500

    Front Page Fixes
    -Implemented the carousel;
    -Fixed the first two post samples (no longer call by excerpts but by
    content)
    -Also did some accompanying style changes.

--- theme/nova/front-page.php
+++ theme/nova/front-page.php
@@ -7,22 +7,22 @@ get_header(); ?>
 		<div id="primary">
 			<div id="content" role="main">
 				<div id="slider">
-					<p>Carousel</p>
+					<?php echo do_shortcode( '[jj-ngg-jquery-carousel html_id="about-jcarousel" gallery="1" gap="10" width="150" height="100" visible="4" scroll="1" auto="03"]' ); ?>
 				</div><!-- #carousel -->
 				<div id="latest-update">
-					<?php query_posts('showposts=1');
-    	  			global $dlPageId;
-      				$dlPageId = null; ?>
-    				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-					<?php $dlPageId = $post->ID; ?>
-                    <h1 class="front-blog-title">
-                    	<h2><?php the_title(); ?></h2>
-                    </h1>
-   					<div id="front-excerpt">
-   						<?php the_content(); ?>
-					</div>
-					<?php endwhile; endif; ?>
-					<?php edit_post_link('Edit this entry.', '', ''); ?>	
+					<?php
+					$args = array( 'numberposts' => 1, 'offset'=> 0, );
+					$postslist = get_posts( $args );
+					foreach ($postslist as $post) :  setup_postdata($post); ?> 
+						<div>
+							<h1 class="front-blog-title"> 
+								<?php the_title(); ?>
+							</h1>
+							<div id="front-excerpt">
+								<?php the_content( $more_link_text ); ?>
+							</div><!-- #front-excerpt -->	
+						</div>
+				<?php endforeach; ?>	
 				</div><!-- #latest -->
 				<div id="next-update">
 				<?php
@@ -34,7 +34,7 @@ get_header(); ?>
 								<?php the_title(); ?>
 							</h1>
 							<div id="front-excerpt">
-								<?php the_excerpt(); ?>
+								<?php the_content( $more_link_text ); ?>
 							</div><!-- #front-excerpt -->	
 						</div>
 				<?php endforeach; ?>
