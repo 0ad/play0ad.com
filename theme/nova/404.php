e8162bbd61eea834b965ce42c40ab46762e45d17
Geek377 <geek377@gmail.com>
2012-05-09 22:02:58 -0500

    CSS Optimizations + Theme Updates
    Added humans.txt, fixed header in 404.php

--- theme/nova/404.php
+++ theme/nova/404.php
@@ -3,8 +3,8 @@
  * The template for displaying 404 pages (Not Found).
  *
  * @package WordPress
- * @subpackage Twenty_Eleven
- * @since Twenty Eleven 1.0
+ * @subpackage WildfireGames
+ * @since Nova 0.1
  */
 
 get_header(); ?>
