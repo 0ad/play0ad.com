06eda396663299a49b9a3e5601795c64ddc27349
Geek377 <geek377@gmail.com>
2012-06-15 14:37:44 -0500

    News Template + Minor Style  Adjustments
    Reconstructed front page based on compromise (still needs cutoff
    point), customized "read more tag", restyled global navigation, looked
    a bit awkward.

--- theme/nova/front-page.php
+++ theme/nova/front-page.php
@@ -9,7 +9,7 @@ get_header(); ?>
 				<div id="carousel">
 					<?php echo do_shortcode( '[jj-ngg-jquery-carousel html_id="about-jcarousel" gallery="1" gap="15" width="205" height="115" visible="3" scroll="1" auto="0" skin_class="jcarousel-skin-tango" wrap="both"]' ); ?>
 				</div><!-- #carousel -->
-				<div id="latest-update">
+				<div class="front-update">
 					<?php
 					$args = array( 'numberposts' => 1, 'offset'=> 0, );
 					$postslist = get_posts( $args );
@@ -19,12 +19,12 @@ get_header(); ?>
 								<?php the_title(); ?>
 							</h1>
 							<div id="front-excerpt">
-								<?php the_content( $more_link_text ); ?>
+								<?php the_content( $more_link_text , $strip_teaser ); ?> 
 							</div><!-- #front-excerpt -->	
 						</div>
 				<?php endforeach; ?>	
 				</div><!-- #latest -->
-				<div id="next-update">
+				<div class="front-update">
 				<?php
 					$args = array( 'numberposts' => 1, 'offset'=> 1,);
 					$postslist = get_posts( $args );
@@ -34,7 +34,7 @@ get_header(); ?>
 								<?php the_title(); ?>
 							</h1>
 							<div id="front-excerpt">
-								<?php the_content( $more_link_text ); ?>
+								<?php the_content( $more_link_text , $strip_teaser ); ?> 
 							</div><!-- #front-excerpt -->	
 						</div>
 				<?php endforeach; ?>
