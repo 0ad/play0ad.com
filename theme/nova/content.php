248a5b37f2cb646321ef85d46382e57194b12971
Brian Stempin <brian.stempin@gmail.com>
2012-06-17 16:26:44 -0400

    Modified the front page to use the content.php template, and modified the content.php template to fix display bugs and to be more reusable.

--- theme/nova/content.php
+++ theme/nova/content.php
@@ -19,12 +19,6 @@
 			<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'nova' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 			<?php endif; ?>
 
-			<?php if ( 'post' == get_post_type() ) : ?>
-			<div class="entry-meta">
-				<?php nova_posted_on(); ?>
-			</div><!-- .entry-meta -->
-			<?php endif; ?>
-
 			<?php if ( comments_open() && ! post_password_required() ) : ?>
 			<div class="comments-link">
 				<?php comments_popup_link( '<span class="leave-reply">' . __( 'Reply', 'nova' ) . '</span>', _x( '1', 'comments number', 'nova' ), _x( '%', 'comments number', 'nova' ) ); ?>
@@ -36,7 +30,13 @@
 		<div class="entry-summary">
 			<?php the_excerpt(); ?>
 		</div><!-- .entry-summary -->
-		<?php else : ?>
+		<?php else : ?>	
+			<?php if ( 'post' == get_post_type() ) : ?>
+			<div class="entry-meta">
+				<?php nova_posted_on(); ?>
+			</div><!-- .entry-meta -->
+			<?php endif; ?>
+			
 		<div class="entry-content">
 			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'nova' ) ); ?>
 			<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'nova' ) . '</span>', 'after' => '</div>' ) ); ?>
--- theme/nova/front-page.php
+++ theme/nova/front-page.php
@@ -1,46 +1,46 @@
 <?php
 /*
-Front page template file
+ Front page template file
 */
 
 get_header(); ?>
-		<div id="primary">
-			<div id="content" role="main">
-				<div id="carousel">
-					<?php echo do_shortcode( '[jj-ngg-jquery-carousel html_id="about-jcarousel" gallery="1" gap="15" width="205" height="115" visible="3" scroll="1" auto="0" skin_class="jcarousel-skin-tango" wrap="both"]' ); ?>
-				</div><!-- #carousel -->
-				<div id="latest-update">
-					<?php
-					$args = array( 'numberposts' => 1, 'offset'=> 0, );
-					$postslist = get_posts( $args );
-					foreach ($postslist as $post) :  setup_postdata($post); ?> 
-						<div>
-							<h1 class="front-blog-title"> 
-								<?php the_title(); ?>
-							</h1>
-							<div id="front-excerpt">
-								<?php the_content( $more_link_text ); ?>
-							</div><!-- #front-excerpt -->	
-						</div>
-				<?php endforeach; ?>	
-				</div><!-- #latest -->
-				<div id="next-update">
-				<?php
-					$args = array( 'numberposts' => 1, 'offset'=> 1,);
-					$postslist = get_posts( $args );
-					foreach ($postslist as $post) :  setup_postdata($post); ?> 
-						<div>
-							<h1 class="front-blog-title"> 
-								<?php the_title(); ?>
-							</h1>
-							<div id="front-excerpt">
-								<?php the_content( $more_link_text ); ?>
-							</div><!-- #front-excerpt -->	
-						</div>
-				<?php endforeach; ?>
-				</div><!-- #next-update -->					
-			</div><!-- #content -->
-		</div><!-- #primary -->
+<div id="primary">
+	<div id="content" role="main">
+		<div id="carousel">
+			<?php echo do_shortcode( '[jj-ngg-jquery-carousel html_id="about-jcarousel" gallery="1" gap="15" width="205" height="115" visible="3" scroll="1" auto="0" skin_class="jcarousel-skin-tango" wrap="both"]' ); ?>
+		</div>
+		<!-- #carousel -->
+		<div id="latest-update">
+			<?php
+			$args = array( 'numberposts' => 1, 'offset'=> 0, );
+			$postslist = get_posts( $args );
+					foreach ($postslist as $post) :  setup_postdata($post); ?>
+			<?php
+			global $more;
+			$more = 0;
+			?>
+			<?php get_template_part( 'content', get_post_format() ); ?>
+			<?php endforeach; ?>
+		</div>
+		<!-- #latest -->
+		<div id="next-update">
+			<?php
+			$args = array( 'numberposts' => 2, 'offset'=> 1,);
+			$postslist = get_posts( $args );
+					foreach ($postslist as $post) :  setup_postdata($post); ?>
+			<?php
+			global $more;
+			$more = 0;
+			?>
+			<?php get_template_part( 'content', get_post_format() ); ?>
+			<?php endforeach; ?>
+		</div>
+		<!-- #next-update -->
+		
+	</div>
+	<!-- #content -->
+</div>
+<!-- #primary -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
