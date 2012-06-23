8d5e4208213c76cbbc423cb27769ff7c99cd7ad9
Geek377 <geek377@gmail.com>
2012-06-23 12:16:55 -0500

    Remove Redundancy Front Page PHP
    It was just bothering me that there were two separate divs holding
    posts. Now there is just one. Also removed deprecated CSS

--- theme/nova/front-page.php
+++ theme/nova/front-page.php
@@ -10,9 +10,9 @@ get_header(); ?>
 			<?php echo do_shortcode( '[jj-ngg-jquery-carousel html_id="about-jcarousel" gallery="1" gap="15" width="205" height="115" visible="3" scroll="1" auto="0" skin_class="jcarousel-skin-tango" wrap="both"]' ); ?>
 		</div>
 		<!-- #carousel -->
-		<div id="latest-update">
+		<div id="updates">
 			<?php
-			$args = array( 'numberposts' => 1, 'offset'=> 0, );
+			$args = array( 'numberposts' => 3, 'offset'=> 0, );
 			$postslist = get_posts( $args );
 					foreach ($postslist as $post) :  setup_postdata($post); ?>
 			<?php
@@ -22,20 +22,7 @@ get_header(); ?>
 			<?php get_template_part( 'content', get_post_format() ); ?>
 			<?php endforeach; ?>
 		</div>
-		<!-- #latest -->
-		<div id="next-update">
-			<?php
-			$args = array( 'numberposts' => 2, 'offset'=> 1,);
-			$postslist = get_posts( $args );
-					foreach ($postslist as $post) :  setup_postdata($post); ?>
-			<?php
-			global $more;
-			$more = 0;
-			?>
-			<?php get_template_part( 'content', get_post_format() ); ?>
-			<?php endforeach; ?>
-		</div>
-		<!-- #next-update -->
+		<!-- #updates -->
 		
 	</div>
 	<!-- #content -->
