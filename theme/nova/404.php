f533d975039b821b226d9b546c364eaa73aa2b59
Geek377 <geek377@gmail.com>
2012-07-23 14:11:17 -0500

    Quick Fixes
    404 ( replaced twentyeleven with nova)
    archive-page( optimized categories)

--- theme/nova/404.php
+++ theme/nova/404.php
@@ -14,18 +14,18 @@ get_header(); ?>
 
 			<article id="post-0" class="post error404 not-found">
 				<header class="entry-header">
-					<h1 class="entry-title"><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'twentyeleven' ); ?></h1>
+					<h1 class="entry-title"><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'nova' ); ?></h1>
 				</header>
 
 				<div class="entry-content">
-					<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching, or one of the links below, can help.', 'twentyeleven' ); ?></p>
+					<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching, or one of the links below, can help.', 'nova' ); ?></p>
 
 					<?php get_search_form(); ?>
 
 					<?php the_widget( 'WP_Widget_Recent_Posts', array( 'number' => 10 ), array( 'widget_id' => '404' ) ); ?>
 
 					<div class="widget">
-						<h2 class="widgettitle"><?php _e( 'Most Used Categories', 'twentyeleven' ); ?></h2>
+						<h2 class="widgettitle"><?php _e( 'Most Used Categories', 'nova' ); ?></h2>
 						<ul>
 						<?php wp_list_categories( array( 'orderby' => 'count', 'order' => 'DESC', 'show_count' => 1, 'title_li' => '', 'number' => 10 ) ); ?>
 						</ul>
@@ -33,7 +33,7 @@ get_header(); ?>
 
 					<?php
 					/* translators: %1$s: smilie */
-					$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'twentyeleven' ), convert_smilies( ':)' ) ) . '</p>';
+					$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'nova' ), convert_smilies( ':)' ) ) . '</p>';
 					the_widget( 'WP_Widget_Archives', array('count' => 0 , 'dropdown' => 1 ), array( 'after_title' => '</h2>'.$archive_content ) );
 					?>
 
--- theme/nova/archive-page.php
+++ theme/nova/archive-page.php
@@ -3,11 +3,6 @@
  * Template Name: Archives
  * The template for displaying Archive pages.
  *
- * Used to display archive-type pages if nothing more specific matches a query.
- * For example, puts together date-based pages if no date.php file exists.
- *
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
  * @package WildfireGames
  * @subpackage Nova
  * @since Nova 0.4
@@ -25,15 +20,15 @@ get_header(); ?>
 						
 						<?php the_post(); the_content();  ?>
 						
-						<h2>Monthly Archive</h2>   
+						<h2>Monthly Archives</h2>   
 						<ul>
 						<?php wp_get_archives( 'type=monthly&format=html&show_post_count=1' ); ?>
 						</ul>
 						<br> 
 					
-						<h2>Categories Archive</h2>
+						<h2>Most Used Categories</h2>
 						<ul>  
-						<?php wp_list_categories('show_count=1'); ?>   
+						<?php wp_list_categories( array( 'orderby' => 'count', 'order' => 'DESC', 'show_count' => 1, 'title_li' => '' ) ); ?>   
 						</ul>
 						<br>
 						 
