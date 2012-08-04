07b2cb3be07620ae113938e2afcc3792d4a018dd
Geek377 <geek377@gmail.com>
2012-08-04 18:56:37 -0500

    Continued Fine Tuning/Template Reorganizing
    Simplifying the templates themselves. And some CSS fixes.

--- theme/nova/content-single.php
+++ theme/nova/content-single.php
@@ -7,60 +7,61 @@
  * @since Nova 0.2
  */
 ?>
-	<header>
-		<h1 class="blog-title"><?php the_title(); ?></h1>
-		<?php if ( 'post' == get_post_type() ) : ?>
-		<?php endif; ?>
-	</header>
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="entry-content">
-		<?php the_content(); ?>
+		<header class="entry-header">
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+			<?php if ( 'post' == get_post_type() ) : ?>
+			<?php endif; ?>
+			<div class="entry-meta">
+				<?php nova_posted_on(); ?> in <?php the_category(', ') ?>
+			</div><!-- .entry-meta -->
+		</header>
+		<div class="entry-content">
+			<?php the_content(); ?>
 		<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'nova' ) . '</span>', 'after' => '</div>' ) ); ?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-meta">
-		<?php nova_posted_on(); ?>
-		<?php
-			/* translators: used between list items, there is a space after the comma */
-			$categories_list = get_the_category_list( __( ', ', 'nova' ) );
-
-			/* translators: used between list items, there is a space after the comma */
-			$tag_list = get_the_tag_list( '', __( ', ', 'nova' ) );
-			if ( '' != $tag_list ) {
-				$utility_text = __( 'This entry was posted in %1$s and tagged %2$s by <a href="%6$s">%5$s</a>. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'nova' );
-			} elseif ( '' != $categories_list ) {
-				$utility_text = __( 'This entry was posted in %1$s by <a href="%6$s">%5$s</a>. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'nova' );
-			} else {
-				$utility_text = __( 'This entry was posted by <a href="%6$s">%5$s</a>. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'nova' );
-			}
-
-			printf(
-				$utility_text,
-				$categories_list,
-				$tag_list,
-				esc_url( get_permalink() ),
-				the_title_attribute( 'echo=0' ),
-				get_the_author(),
-				esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) )
-			);
-		?>
-		<?php edit_post_link( __( 'Edit', 'nova' ), '<span class="edit-link">', '</span>' ); ?>
-
-		<?php if ( get_the_author_meta( 'description' ) && ( ! function_exists( 'is_multi_author' ) || is_multi_author() ) ) : // If a user has filled out their description and this is a multi-author blog, show a bio on their entries ?>
-		<div id="author-info">
-			<div id="author-avatar">
-				<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'nova_author_bio_avatar_size', 68 ) ); ?>
-			</div><!-- #author-avatar -->
-			<div id="author-description">
-				<h2><?php printf( __( 'About %s', 'nova' ), get_the_author() ); ?></h2>
-				<?php the_author_meta( 'description' ); ?>
-				<div id="author-link">
-					<a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
-						<?php printf( __( 'View all posts by %s <span class="meta-nav">&rarr;</span>', 'nova' ), get_the_author() ); ?>
-					</a>
-				</div><!-- #author-link	-->
-			</div><!-- #author-description -->
-		</div><!-- #entry-author-info -->
-		<?php endif; ?>
-	</footer><!-- .entry-meta -->
-</article><!-- #post-<?php the_ID(); ?> -->
+		</div><!-- .entry-content -->
+			<footer class="entry-meta">
+				<?php
+					/* translators: used between list items, there is a space after the comma */
+					$categories_list = get_the_category_list( __( ', ', 'nova' ) );
+		
+					/* translators: used between list items, there is a space after the comma */
+					$tag_list = get_the_tag_list( '', __( ', ', 'nova' ) );
+					if ( '' != $tag_list ) {
+						$utility_text = __( 'This entry was posted in %1$s and tagged %2$s by <a href="%6$s">%5$s</a>. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'nova' );
+					} elseif ( '' != $categories_list ) {
+						$utility_text = __( 'This entry was posted in %1$s by <a href="%6$s">%5$s</a>. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'nova' );
+					} else {
+						$utility_text = __( 'This entry was posted by <a href="%6$s">%5$s</a>. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'nova' );
+					}
+		
+					printf(
+						$utility_text,
+						$categories_list,
+						$tag_list,
+						esc_url( get_permalink() ),
+						the_title_attribute( 'echo=0' ),
+						get_the_author(),
+						esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) )
+					);
+				?>
+				<?php edit_post_link( __( 'Edit', 'nova' ), '<span class="edit-link">', '</span>' ); ?>
+		
+				<?php if ( get_the_author_meta( 'description' ) && ( ! function_exists( 'is_multi_author' ) || is_multi_author() ) ) : // If a user has filled out their description and this is a multi-author blog, show a bio on their entries ?>
+				<div id="author-info">
+					<div id="author-avatar">
+						<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'nova_author_bio_avatar_size', 68 ) ); ?>
+					</div><!-- #author-avatar -->
+					<div id="author-description">
+						<h2><?php printf( __( 'About %s', 'nova' ), get_the_author() ); ?></h2>
+						<?php the_author_meta( 'description' ); ?>
+						<div id="author-link">
+							<a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
+								<?php printf( __( 'View all posts by %s <span class="meta-nav">&rarr;</span>', 'nova' ), get_the_author() ); ?>
+							</a>
+						</div><!-- #author-link	-->
+					</div><!-- #author-description -->
+				</div><!-- #author-info -->
+				<?php endif; ?>
+			</footer><!-- .entry-meta -->
+	</article><!-- #post-<?php the_ID(); ?> -->
