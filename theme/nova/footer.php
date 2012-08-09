9b2e6685c8250d318def228a0d1e542ab374da85
Geek377 <geek377@gmail.com>
2012-08-09 12:33:14 -0500

    Added Modernizer
    And deleted the local Paypal image, caused issues

--- theme/nova/footer.php
+++ theme/nova/footer.php
@@ -9,7 +9,7 @@
 ?>
 	</div><!-- #main -->
 	<footer id="colophon" role="contentinfo">
-		<div id="supplementary" class="five">
+		<div id="supplementary">
 			<?php dynamic_sidebar( 'sidebar-3' ); ?>
 		</div>
 		<div id="site-generator">
@@ -20,9 +20,10 @@
 </div><!-- #page -->
 <?php wp_footer(); ?>
 <script>
-        var _gaq=[['_setAccount','UA-33895773-1'],['_trackPageview']];
-        (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-        g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-        s.parentNode.insertBefore(g,s)}(document,'script'));</script>
+	var _gaq=[['_setAccount','UA-33895773-1'],['_trackPageview']];
+	(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g,s)}(document,'script'));
+</script>
 </body>
 </html>
--- theme/nova/header.php
+++ theme/nova/header.php
@@ -6,29 +6,26 @@
 * @subpackage Nova
 * @since Nova 0.2
 */
-?><!DOCTYPE html>
-<!--[if IE 6]>
-<html id="ie6" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if IE 7]>
-<html id="ie7" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if IE 8]>
-<html id="ie8" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
-<html <?php language_attributes(); ?>>
-<!--<![endif]-->
+?>
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html dir="ltr" lang="en-US" class="no-js ie ie6 lte7 lte8 lte9" <?php language_attributes(); ?>>><![endif]-->
+<!--[if IE 7 ]><html dir="ltr" lang="en-US" class="no-js ie ie7 lte7 lte8 lte9" <?php language_attributes(); ?>>><![endif]-->
+<!--[if IE 8 ]><html dir="ltr" lang="en-US" class="no-js ie ie8 lte8 lte9" <?php language_attributes(); ?>>><![endif]-->
+<!--[if IE 9 ]><html dir="ltr" lang="en-US" class="no-js ie ie9 lte9" <?php language_attributes(); ?>>><![endif]-->
+<!--[if (gt IE 9)|!(IE)]><html dir="ltr" lang="en-US" class="no-js"><![endif]-->
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta http-equiv="imagetoolbar" content="false">
 	<meta name="viewport" content="width=device-width" />
 	<title><?php bloginfo('name'); ?> | A free, open-source game of ancient warfare</title>
 	<link rel="author" href="/humans.txt">
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-	<link href='http://fonts.googleapis.com/css?family=Gentium+Basic:400,700&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+	<link rel="dns-prefetch" href="s.ytimg.com">
+	<link rel="dns-prefetch" href="ajax.googleapis.com">
+	<script src="/js/modernizr.js"></script>
 	<?php wp_head(); ?>
 </head>
 
@@ -37,7 +34,7 @@
 <div id="page" class="hfeed">
 	<div id="global-bar">
 		<nav id="global" role="navigation">
-			<?php wp_nav_menu( array('menu' => 'GLOBAL', 'container' => '' , 'menu_class' => 'GLOBAL', 'menu_id' =>'' )); ?>
+			<?php wp_nav_menu( array('menu' => 'GLOBAL')); ?>
 		</nav>
 	</div>
 	<header id="branding" role="banner">
@@ -55,7 +52,7 @@
        	<div id="navbar">
        		<a href="<?php bloginfo('url'); ?>"><div id="home-btn">Home</div></a>
        		<nav id="access" role="navigation">
-       			<?php wp_nav_menu( array( 'menu' => 'MAIN' ) ); ?>
+       			<?php wp_nav_menu( array( 'menu' => 'MAIN')); ?>
        		</nav><!-- #access --> 
        	</div>				
 	</header><!-- #branding -->
