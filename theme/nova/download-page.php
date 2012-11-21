90d122c080ece85b31098f45152a3ff5504b3e80
Geek377 <geek377@gmail.com>
2012-11-21 14:26:17 -0600

    Basic Download Template

--- theme/nova/download-page.php
+++ theme/nova/download-page.php
@@ -11,20 +11,27 @@
 get_header(); ?>
 
 		<div id="full">
-			<div id="content" role="main">
-				
-				<div id="dl-banner">
-					<div id="dl-button">
-						<div id="dl-title">Download</div>
-						<div id="dl-description"><?php bloginfo('description'); ?></div>
-					</div><!-- Download Button -->
-				</div><!-- Download Banner -->
-				
-				<!-- Page content below banner -->
-				<?php while ( have_posts() ) : the_post(); ?>
-					<?php get_template_part( 'content', 'page' ); ?>
-				<?php endwhile; ?>
-			
+			<div id="content" role="main">				
+				<article class="hentry">
+					<header class="entry-header">
+						<div class="entry-tri"></div>
+						<h1 class="entry-title"><?php the_title(); ?></h1>
+					</header><!-- .entry-header -->
+					<div class="entry-content">
+					
+						<div id="dl-banner">
+							<div id="dl-page-button">
+								<h1 id="dl-title">Download</h1>
+								<div id="dl-description">
+									<?php bloginfo('description'); ?> <div id="dl-tag"></div><a href="http://trac.wildfiregames.com/wiki/LatestRelease">Other versions...</a>
+								</div>
+							</div><!-- Download Button -->
+						</div><!-- Download Banner -->
+						 	
+						<?php the_post(); the_content();  ?>
+						
+					</div><!-- #entry-content -->
+				</article><!-- .hentry -->
 			</div><!-- #content -->
 		</div><!-- #full -->
 		
