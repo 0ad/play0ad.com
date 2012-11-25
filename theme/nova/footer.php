56cbad3ded2217e3933366e9be6b2abb5af13279
Geek377 <geek377@gmail.com>
2012-11-25 10:35:53 -0600

    Remove GA from dev instances

--- theme/nova/footer.php
+++ theme/nova/footer.php
@@ -28,11 +28,5 @@
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 <?php wp_footer(); ?>
-<script>
-	var _gaq=[['_setAccount','UA-33895773-1'],['_trackPageview']];
-	(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-    s.parentNode.insertBefore(g,s)}(document,'script'));
-</script>
 </body>
 </html>
